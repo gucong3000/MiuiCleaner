bb5edbe04532222fbc8ab28c6522fdd394c79d57
刘祺 <gucong@gmail.com>
2022-10-11 01:10:14 +0800

    更新讯飞下载地址

--- src/miui_cleaner_app/downApp.js
+++ src/miui_cleaner_app/downApp.js
@@ -12,8 +12,8 @@ const appList = [
 	{
 		name: "李跳跳",
 		summary: "干净小巧的广告自动跳过工具",
-		packageName: "cn.litiaotiao.app",
 		icon: "https://litiaotiao.cn/apple-touch-icon.png",
+		packageName: "cn.litiaotiao.app",
 		url: [
 			"https://litiaotiao.cn/",
 			"https://www.123pan.com/s/A6cA-edAJh",
@@ -24,8 +24,8 @@ const appList = [
 	{
 		name: "Edge",
 		summary: "浏览器，微软出品，带广告屏蔽功能",
-		packageName: "com.microsoft.emmx",
 		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/083ca7b4c3bc34d6ca687789daa6747ea368d8747",
+		packageName: "com.microsoft.emmx",
 		url: [
 			"https://www.coolapk.com/apk/com.microsoft.emmx",
 			"https://app.mi.com/details?id=com.microsoft.emmx",
@@ -34,8 +34,8 @@ const appList = [
 	{
 		name: "小米浏览器",
 		summary: "国际版",
-		packageName: "com.mi.globalbrowser",
 		icon: "http://img.itmop.com/upload/2018-2/2018227171959917.png",
+		packageName: "com.mi.globalbrowser",
 		url: [
 			"https://wwm.lanzouj.com/idzsf0bh062h",
 			"https://www.firepx.com/app/android-mi-browser-google-play/",
@@ -45,9 +45,13 @@ const appList = [
 	{
 		name: "讯飞输入法",
 		summary: "定制版、Google Play版",
-		packageName: "com.iflytek.inputmethod",
-		url: "https://423down.lanzouv.com/b0f24av5i",
 		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/04f6d4e16c4c41e570c4daf8349ad32f1e342cb66",
+		packageName: "com.iflytek.inputmethod",
+		url: [
+			"https://firepx.lanzoul.com/b00vf92jc#\n密码:647w",
+			// "https://app.meizu.com/apps/public/detail?package_name=com.iflytek.inputmethod",
+			"https://423down.lanzouv.com/b0f24av5i",
+		],
 		// [
 		// 	"https://www.coolapk.com/apk/com.iflytek.inputmethod",
 		// 	"https://app.mi.com/details?id=com.iflytek.inputmethod",
@@ -57,6 +61,7 @@ const appList = [
 		name: "软件包安装程序",
 		summary: "Google版",
 		packageName: "com.google.android.packageinstaller",
+		icon: "https://file.1xiazai.net/d/file/android/20220728/202266164286724.png",
 		url: {
 			// 3.01 MB 版本号 未知 适用于安卓 13 SDK 33
 			33: "https://www.123pan.com/s/OZe0Vv-iOKl3",
@@ -73,14 +78,15 @@ const appList = [
 	{
 		name: "应用包管理组件",
 		summary: "MIUI软件包安装程序v3.8.0，不含“纯净模式”",
+		icon: "https://img.1xiazai.net/d/file/android/20220407/2021731123154350.png",
 		packageName: "com.miui.packageinstaller",
 		url: "https://zisu.lanzoum.com/iI7LGwn5xjc",
 	},
 	{
 		name: "QQ音乐简洁版",
 		summary: "v3.8.0 MIUI 音乐APP套壳的产品",
-		packageName: "com.tencent.qqmusiclite",
 		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/0f487cb7981174cf5a5338085d4d4acd5b1279946",
+		packageName: "com.tencent.qqmusiclite",
 		url: [
 			"https://www.coolapk.com/apk/com.tencent.qqmusiclite",
 			"https://app.mi.com/details?id=com.tencent.qqmusiclite",
@@ -89,8 +95,8 @@ const appList = [
 	{
 		name: "Holi天气",
 		summary: "干净、小巧、漂亮、功能多",
-		packageName: "com.joe.holi",
 		icon: "http://pp.myapp.com/ma_icon/0/icon_42394164_1637810437/128",
+		packageName: "com.joe.holi",
 		url: [
 			"https://www.coolapk.com/apk/com.joe.holi",
 			"https://app.mi.com/details?id=com.joe.holi",
@@ -99,58 +105,58 @@ const appList = [
 	{
 		name: "ES文件浏览器",
 		summary: "去广告版，替代MIUI视频、音乐、文档查看器",
-		packageName: "com.estrongs.android.pop",
 		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/05b6f55415e9941468fd3185ebe8d80816335f599",
+		packageName: "com.estrongs.android.pop",
 		url: "https://423down.lanzouv.com/b0f1d7s2h",
 	},
 	{
 		name: "WPS Office Lite",
 		summary: "国际版，无广告，替代“文档查看器”",
-		packageName: "cn.wps.moffice_i18n",
 		icon: "https://findorra.com/apps/app/images/appicons/85525195-e34c-4314-a771-454e60c2e7de.PNG",
+		packageName: "cn.wps.moffice_i18n",
 		url: "https://www.32r.com/app/109976.html",
 	},
 	{
 		name: "知乎",
 		summary: "集成“知了”，“设置→知了”中有去广告开关",
-		packageName: "com.zhihu.android",
 		icon: "https://static.zhihu.com/heifetz/assets/apple-touch-icon-60.8f6c52aa.png",
+		packageName: "com.zhihu.android",
 		url: "https://423down.lanzouo.com/b0f2lkafe",
 		// url: "www.baidu.com",
 	},
 	{
 		name: "哔哩哔哩",
 		summary: "“设置→哔哩漫游→关于版本”点五下有惊喜",
-		packageName: "tv.danmaku.bili",
 		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/096ba00f21e7f4633b0cbc9e596b5ab5233f608b5",
+		packageName: "tv.danmaku.bili",
 		url: "https://423down.lanzouv.com/b0f1gksne",
 	},
 	{
 		name: "优酷视频",
 		summary: "去广告版",
-		packageName: "com.youku.phone",
 		icon: "https://img.alicdn.com/tfs/TB1WeJ9Xrj1gK0jSZFuXXcrHpXa-195-195.png",
+		packageName: "com.youku.phone",
 		url: "https://423down.lanzouv.com/b0f1avpib",
 	},
 	{
 		name: "百度贴吧",
 		summary: "去广告版",
-		packageName: "com.baidu.tieba",
 		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/02f3ec586a44940620835c58ff9b3c760914d1725",
+		packageName: "com.baidu.tieba",
 		url: "https://423down.lanzouv.com/b0f1b6q8d",
 	},
 	{
 		name: "酷安",
 		summary: "应用商店，去广告版",
-		packageName: "com.coolapk.market",
 		icon: "https://static.coolapk.com/static/web/v8/images/header-logo.png",
+		packageName: "com.coolapk.market",
 		url: "https://423down.lanzouv.com/b0f2uzq2b",
 	},
 	{
 		name: "AppShare",
 		summary: "应用商店，刷机包，国际版提取的APP",
-		packageName: "info.muge.appshare",
 		icon: "http://pic.xfdown.com/uploads/2022-5/2022551511344265.png",
+		packageName: "info.muge.appshare",
 		url: "https://423down.lanzouv.com/iHmmD06tw9xa",
 	},
 ];
