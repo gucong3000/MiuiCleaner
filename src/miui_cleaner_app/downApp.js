7a2f290529c0beba59e20ef2256764af1ced610a
刘祺 <gucong@gmail.com>
2022-10-11 21:49:00 +0800

    app在线图标改为魅族商店链接

--- src/miui_cleaner_app/downApp.js
+++ src/miui_cleaner_app/downApp.js
@@ -24,7 +24,7 @@ const appList = [
 	{
 		name: "Edge",
 		summary: "浏览器，微软出品，带广告屏蔽功能",
-		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/083ca7b4c3bc34d6ca687789daa6747ea368d8747",
+		icon: "http://i5.res.meizu.com/fileserver/app_icon/11841/3ea088fedc65436b84b718170cef7738.png",
 		packageName: "com.microsoft.emmx",
 		url: [
 			"https://www.coolapk.com/apk/com.microsoft.emmx",
@@ -45,7 +45,7 @@ const appList = [
 	{
 		name: "讯飞输入法",
 		summary: "定制版、Google Play版",
-		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/04f6d4e16c4c41e570c4daf8349ad32f1e342cb66",
+		icon: "http://i5.res.meizu.com/fileserver/app_icon/11859/6866676385f64b329f56fe51731e23d3.png",
 		packageName: "com.iflytek.inputmethod",
 		url: [
 			"https://firepx.lanzoul.com/b00vf92jc#\n密码:647w",
@@ -85,7 +85,7 @@ const appList = [
 	{
 		name: "QQ音乐简洁版",
 		summary: "v3.8.0 MIUI 音乐APP套壳的产品",
-		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/0f487cb7981174cf5a5338085d4d4acd5b1279946",
+		icon: "http://i5.res.meizu.com/fileserver/app_icon/11507/2d07011365854d2c83832165e3e0d547.",
 		packageName: "com.tencent.qqmusiclite",
 		url: [
 			"https://www.coolapk.com/apk/com.tencent.qqmusiclite",
@@ -95,7 +95,7 @@ const appList = [
 	{
 		name: "Holi天气",
 		summary: "干净、小巧、漂亮、功能多",
-		icon: "http://pp.myapp.com/ma_icon/0/icon_42394164_1637810437/128",
+		icon: "http://i5.res.meizu.com/fileserver/app_icon/11520/a7affd641ea44a0789bf39daef5998c7.png",
 		packageName: "com.joe.holi",
 		url: [
 			"https://www.coolapk.com/apk/com.joe.holi",
@@ -105,7 +105,7 @@ const appList = [
 	{
 		name: "ES文件浏览器",
 		summary: "去广告版，替代MIUI视频、音乐、文档查看器",
-		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/05b6f55415e9941468fd3185ebe8d80816335f599",
+		icon: "http://i5.res.meizu.com/fileserver/app_icon/11477/a504f85503544b27b9b93090a3eede5c.png",
 		packageName: "com.estrongs.android.pop",
 		url: "https://423down.lanzouv.com/b0f1d7s2h",
 	},
@@ -127,7 +127,7 @@ const appList = [
 	{
 		name: "哔哩哔哩",
 		summary: "“设置→哔哩漫游→关于版本”点五下有惊喜",
-		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/096ba00f21e7f4633b0cbc9e596b5ab5233f608b5",
+		icon: "http://i5.res.meizu.com/fileserver/app_icon/11862/778e4f1b19364fcba562557f240fa0cb.png",
 		packageName: "tv.danmaku.bili",
 		url: "https://423down.lanzouv.com/b0f1gksne",
 	},
@@ -141,7 +141,7 @@ const appList = [
 	{
 		name: "百度贴吧",
 		summary: "去广告版",
-		icon: "http://file.market.xiaomi.com/thumbnail/PNG/l114/AppStore/02f3ec586a44940620835c58ff9b3c760914d1725",
+		icon: "http://i5.res.meizu.com/fileserver/app_icon/11862/21ad75e2cddb49dc8753ed788952c470.png",
 		packageName: "com.baidu.tieba",
 		url: "https://423down.lanzouv.com/b0f1b6q8d",
 	},
